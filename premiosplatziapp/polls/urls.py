db87bb2414f877fac85ff3beafb550ff6845e1b3
Ariel <alarmoreira@gmail.com>
2022-09-19 19:31:15 -0300

    Se agregan vistas basadas en clases

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -4,13 +4,15 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    #ex: /polls/
-    path('', views.index, name='index'),
-    #ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    #ex: /polls/5/results
-    path('<int:question_id>/results', views.results, name='results'),
-    #ex: /polls/5/votes
+    #     ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    #    ex: /polls/5/
+    #    '<int:question_id>/' -> views based function 
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    #    ex: /polls/5/results
+    #    '<int:question_id>/results' -> views based function 
+    path('<int:pk>/results', views.ResultView.as_view(), name='results'),
+    #    ex: /polls/5/votes
     path('<int:question_id>/votes', views.votes, name='votes'),
 ]
 
