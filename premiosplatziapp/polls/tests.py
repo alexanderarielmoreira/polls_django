cc6409a9dafa66443329687cc6fac09b1ea4d85a
Ariel <alarmoreira@gmail.com>
2022-09-29 19:23:15 -0300

    Se crea un test en polls y se corrige ese error en models.

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from datetime import datetime
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTest(TestCase):
+    def test_was_pusblished_recently_with_future_questions(self):
+        """Was_published_recenty returns false for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="¿Quien es el mejor Course Director de Platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
